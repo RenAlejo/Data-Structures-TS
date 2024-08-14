ed1cd10ca31429b47d4a3f4481ecfad012b04967
Alejandro Rendón <80918483+RenAlejo@users.noreply.github.com>
2024-08-14 18:43:01 -0500

    Queue Structure

---
+++ build/Map.js
@@ -0,0 +1,10 @@
+"use strict";
+const map = new Map([['a', 1], ['b', 2], ['c', 3]]);
+map.set('d', 4);
+map.delete('a');
+console.log(map.has('a'));
+console.log('Size:', map.size);
+map.clear();
+for (const [key, value] of map) {
+    console.log(key, value);
+}
