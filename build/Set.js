6259d8eb31bda696752419487854dec4325fcbd0
Alejandro Rendón <80918483+RenAlejo@users.noreply.github.com>
2024-08-14 16:59:01 -0500

    Set Structure

--- build/Set.js
+++ build/Set.js
@@ -1,10 +1,10 @@
-const set = new Set<number>([1,2,3,4]);
+"use strict";
+const set = new Set([1, 2, 3, 4]);
 set.add(5);
 console.log(set.has(5));
 set.delete(3);
 console.log(set.size);
 set.clear();
-
-for(const item of set){
+for (const item of set) {
     console.log('set: ', item);
 }
